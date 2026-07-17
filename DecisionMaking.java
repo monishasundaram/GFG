7fd1a87f97591413822f7f5460c5c6e03a8cbea0
Monisha <monishasundaram17@gmail.com>
2026-07-17 14:41:15 +0530

    Add DecisionMaking.java for comparing two integers

---
+++ DecisionMaking.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        // code here
+        if(n<m)
+            System.out.println("less");
+        else if(n==m)
+            System.out.println("equal");
+        else
+            System.out.println("greater");
+    }
+}
