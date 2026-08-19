b15b87bfeef3a6b89579f30c1aaa54ff56d4b435
Monisha <monishasundaram17@gmail.com>
2026-08-19 20:07:10 +0530

    Implement countSquares method to count perfect squares

---
+++ CountPerfectSquares.java
@@ -0,0 +1,10 @@
+class Solution {
+    static int countSquares(int n) {
+        // code here
+        int count = 0;
+        for(int i = 1;i*i< n;i++){
+            count++;
+        }
+        return count;
+    }
+}
