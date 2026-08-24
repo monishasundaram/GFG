2b27b6b557ee5f5ba5c90dbea49ce020e0c76b04
Monisha <monishasundaram17@gmail.com>
2026-08-24 15:25:32 +0530

    Add PrintWithSeparator class for formatted output

---
+++ PrintWithSeparator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        String s = sc.nextLine();
+
+        // code here
+        System.out.print(a+s+b);
+    }
+}
