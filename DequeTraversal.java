6b09a667e8777dc93131fda0ab184135005443a4
Monisha <monishasundaram17@gmail.com>
2026-08-25 11:52:32 +0530

    Implement printDeque method to print elements

---
+++ DequeTraversal.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void printDeque(ArrayDeque<Integer> deq) {
+        // code here
+        for (Integer element : deq) {
+             System.out.print(element + " ");
+         }
+         System.out.println();
+    }
+}
