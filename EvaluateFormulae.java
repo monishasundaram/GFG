db4a7d6dfeb413898344c0d73306203aa1f9e9e6
Monisha <monishasundaram17@gmail.com>
2026-08-27 20:56:00 +0530

    Add EvaluateFormulae.java for formula evaluation

---
+++ EvaluateFormulae.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        // code here
+        int s = a+b;
+        int t = s/c;
+        int v = t+d;
+        System.out.print(v);
+    }
+}
