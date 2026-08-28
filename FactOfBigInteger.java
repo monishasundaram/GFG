b6d17724037b577c1d75216f5dffc34d53cb9261
Monisha <monishasundaram17@gmail.com>
2026-08-28 22:19:27 +0530

    Implement factorial method using BigInteger

---
+++ FactOfBigInteger.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+class Solution {
+    public static BigInteger factorial(int n) {
+        BigInteger fact = BigInteger.ONE;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact.multiply(BigInteger.valueOf(i));
+        }
+
+        return fact;
+    }
+}
