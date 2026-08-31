5ae4f425fe39aecb531e2f32565fc59df650d026
Monisha <monishasundaram17@gmail.com>
2026-08-31 20:39:40 +0530

    Implement bitwise shift operations in BitwiseShiftOp

---
+++ BitwiseShiftOp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // code here
+        int c = a >> b;
+        int d = a << b;
+        System.out.println(c+" "+d);
+    }
+}
