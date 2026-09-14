17ba2fe2a3f371919f1abf711f3c3d225a590561
Monisha <monishasundaram17@gmail.com>
2026-09-14 19:24:22 +0530

    Add PrintWithNewLine.java to print multiple lines

---
+++ PrintWithNewLine.java
@@ -0,0 +1,9 @@
+class GFG {
+    public static void main(String[] args) {
+
+        // code here
+        System.out.println("Geeks");
+        System.out.println("for");
+        System.out.println("Geeks");
+    }
+}
