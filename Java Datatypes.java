22921bb12ff2d6b16cedd87df729156bce10e216
Monisha <monishasundaram17@gmail.com>
2026-09-20 21:33:26 +0530

    Implement methods for Java data types

---
+++ Java Datatypes.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    int javaIntType(Scanner sc) {
+        // code here
+        int num = sc.nextInt();
+        return num;
+    }
+
+    String javaStringType(Scanner sc) {
+        
+        // code here
+        String str = sc.next();
+        return str;
+    }
+
+        
+    float javaFloatType(Scanner sc) {
+        // code here
+        Float flt = sc.nextFloat();
+        return flt;
+    }
+};
