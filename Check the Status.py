20f5a5fa6d085472de21832f299b48ebd56e2112
Monisha <monishasundaram17@gmail.com>
2026-09-22 23:21:59 +0530

    Implement checkStatus method in Solution class

---
+++ Check the Status.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkStatus(self, a, b, flag):
+        # code here
+        if ((a>0 and b<0) or (a<0 and b>0)) and flag == False:
+            return True
+        elif a<0 and b<0 and flag == True:
+            return True
+        else:
+            return False
